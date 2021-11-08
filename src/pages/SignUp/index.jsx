d721c56f6bbde5bbdf68f3682ff8e03baee809a1
victordurco <victordurco@gmail.com>
2021-11-08 19:33:38 -0300

    header done

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 
 const SignUp = () => (
   <div>
-    <Header />
+    <Header>
+      <Logo>
+        dev_store
+      </Logo>
+    </Header>
   </div>
 );
 
@@ -13,4 +17,15 @@ const Header = styled.div`
   width: 100%;
   height: 109px;
   background-color: #fa4098;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 4px 4px 0px #00000040;
+
+`;
+
+const Logo = styled.span`
+    font-size: 45px;
+    margin: auto 0 auto 182px;
+    color: white;
+    font-family: 'Quantico', sans-serif;
 `;
