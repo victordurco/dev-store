4054eb4d43a61f2e3947cfa0fcc25ef3badba65a
victordurco <victordurco@gmail.com>
2021-11-10 22:15:48 -0300

    changed service url route to heroku

--- src/services/devStore.services.js
+++ src/services/devStore.services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL_API = 'http://localhost:4000';
+const URL_API = 'https://driven-dev-store.herokuapp.com';
 
 const signIn = (body) => axios.post(`${URL_API}/sign-in`, body);
 
