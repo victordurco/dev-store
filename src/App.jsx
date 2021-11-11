42ed61dda55190fe1d6037739fbf721ff49fc35f
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-10 22:02:09 -0300

    User is redirect to NotFound page if the route doesn't exist

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import GlobalStyle from './styles/GlobalStyle';
 import HomePage from './pages/Home';
 import SignIn from './pages/SignIn';
+import NotFound from './pages/NotFound';
 import UserContext from './contexts/UserContext';
 
 export default function App() {
@@ -33,6 +35,7 @@ export default function App() {
           <Routes>
             <Route path="/" exact element={<HomePage />} />
             <Route path="/entrar" exact element={<SignIn />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
