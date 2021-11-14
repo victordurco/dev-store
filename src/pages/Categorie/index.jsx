3ad037564bee14d589250eb97e7f76aba19563f9
victordurco <victordurco@gmail.com>
2021-11-14 17:37:29 -0300

    send to not found page when user trys an invalid category

--- src/pages/Categorie/index.jsx
+++ src/pages/Categorie/index.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import Swal from 'sweetalert2';
 import styled from 'styled-components';
 import Loader from 'react-loader-spinner';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Container from '../shared/Container';
 import { getCategorieProducts } from '../../services/devStore.services';
 import ProductCard from '../shared/ProductCard';
 
 const Categorie = () => {
   const { categorieId } = useParams();
+  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -18,11 +18,7 @@ const Categorie = () => {
         setProducts(res.data);
         setLoading(false);
       })
-      .catch(() => Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Algo deu errado, por favor recarregue a pagina',
-      }));
+      .catch(() => navigate('/*'));
   }, [categorieId]);
 
   return (
